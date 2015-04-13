5afb65dc83975010952c38dd52eb6b1ce06a0320
Maciej Pawlik <m.pawlik@cyfronet.pl>
2015-04-13 17:52:48 +0200

    Add some initialization code and sendMetrics prototype.

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var MonitoringPlugin = function() {
-
+var MonitoringPlugin = function () {
 };
 
-MonitoringPlugin.prototype.init = function(rcl, wflib, engine) {
+MonitoringPlugin.prototype.sendMetrics = function () {
+    console.log('Im running:', this.engine.nTasksLeft.toString(), 'tasks');
+};
+
+MonitoringPlugin.prototype.init = function (rcl, wflib, engine) {
+    if (this.hasOwnProperty('initialized') && this.initialized === true) {
+        return;
+    }
     this.rcl = rcl;
     this.wflib = wflib;
     this.engine = engine;
 
-    console.log("Hello world!");
+    var that = this;
+    setInterval(function () {
+        that.sendMetrics();
+    }, 1000);
+
+    this.initialized = true;
 };
 
 module.exports = MonitoringPlugin;
