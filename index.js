1a154ed63855cac6c4fb8300923b7729b5e750bc
Maciej Pawlik <m.pawlik@cyfronet.pl>
2015-04-13 18:18:04 +0200

    Add simple implementation of sendMetric with call to getTasksLeft.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+var net = require('net');
+
+var METRIC_COLLECTOR = 'localhost:9001';
+var SERVER = 'HyperFlow';
+
 var MonitoringPlugin = function () {
 };
 
 MonitoringPlugin.prototype.sendMetrics = function () {
-    console.log('Im running:', this.engine.nTasksLeft.toString(), 'tasks');
+    var that = this;
+    //TODO: Create connection once and then try to reuse it
+    var client = net.connect({host: 'localhost', port: 9001}, function () {
+        var metricReport = SERVER + ' nTasksLeft ' + that.getTasksLeft() + ' ' + parseInt(Date.now()/1000) + '\r\n';
+        client.write(metricReport);
+        client.destroy();
+    });
+};
+
+MonitoringPlugin.prototype.getTasksLeft = function () {
+    return this.engine.nTasksLeft;
 };
 
 MonitoringPlugin.prototype.init = function (rcl, wflib, engine) {
